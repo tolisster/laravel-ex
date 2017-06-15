07d90fd8e5fb5e07913f6ac19f772a8e2a8caa90
anatoli Lazar <tolisster@gmail.com>
2017-06-15 21:46:49 +0300

    Test logo

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -22,3 +22,18 @@ Route::get('/home', 'HomeController@index');
 Route::get('/logos', 'LogoController@index');
 Route::post('/logo', 'LogoController@store');
 Route::delete('/logo/{logo}', 'LogoController@destroy');
+
+Route::get('/test', function()
+{
+    $img = Image::canvas(300, 200, '#ff0000');
+
+    $img->text('Alfabravo', 150, 100, function($font) {
+        $font->file(resource_path('assets/fonts/conthrax-sb.ttf'));
+        $font->size(24);
+        $font->color('#fdf6e3');
+        $font->align('center');
+        $font->valign('middle');
+    });
+
+    return $img->response('jpg');
+});
